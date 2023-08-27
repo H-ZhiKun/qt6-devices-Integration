2df230db9fbb0533160ca7e6d212dbba233d02ed
dt <952007485@qq.com>
2023-08-27 22:12:42 +0800

    deal Pgsql and merge main

--- src/sql/PgsqlConnectionPool.cpp
+++ src/sql/PgsqlConnectionPool.cpp
@@ -6,10 +6,10 @@ QSqlDatabase *PgsqlConnectionPool::createConnection()
     QString name = QString("pgsql_connection_") + QString::number(count_);
     QSqlDatabase *db = new QSqlDatabase(QSqlDatabase::addDatabase("QPSQL", name)); // 使用 "QPSQL" 代表 PostgreSQL
     db->setHostName("127.0.0.1");
-    db->setPort(5132); // PostgreSQL 默认端口号是 5132
+    db->setPort(5432); // PostgreSQL 默认端口号是 5132
     db->setDatabaseName("integration");
     db->setUserName("postgres"); // PostgreSQL 默认用户名是 "postgres"
-    db->setPassword("~!dtfs@#"); // 注意使用的密码
+    db->setPassword("123456");   // 注意使用的密码
 
     if (!db->open())
     {
