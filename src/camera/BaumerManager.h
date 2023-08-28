009d98e4949e31b1296a247fb97c6ad00e67e3b7
SunnyLemon-Horace <452174646@qq.com>
2023-08-29 01:42:23 +0800

    new camera version

--- src/camera/BaumerManager.h
+++ src/camera/BaumerManager.h
@@ -12,37 +12,33 @@
 #include <string>
 #include <thread>
 #include <unordered_map>
+#include <yaml-cpp/yaml.h>
 class BaumerManager
 {
   public:
-    explicit BaumerManager(Json::Value jsParam);
+    explicit BaumerManager();
     virtual ~BaumerManager();
-    void start();
+    void start(const YAML::Node &launchConfig);
     void searchCamera(); // 搜索相机
-    bool setCameraParam(const Json::Value &param);
-    Json::Value getCameraParam(const std::string &SN);      // 根据sn号获取对应相机的参数列表
-    std::list<cv::Mat> getImageBySN(const std::string &SN); // 根据sn号获取对应相机的图片
-    std::list<std::string> getCameraList();                 // 返回相机列表sn号
+    bool setCamera(const Json::Value &param, std::string &des);
+    Json::Value getCamera(uint8_t number);           // 根据配置窗口号获取对应相机的参数列表
+    std::list<cv::Mat> getImageBySN(uint8_t number); // 根据sn号获取对应相机的图片
     void removeCamera(const std::string &snNumber);
 
   protected:
     void initializeBGAPI();
     void deinitializeBGAPI();
     void stop();
-    void addCamera(const std::string &snNumber, BGAPI2::Device *dev);
-    void addSNNumber(const std::string &snNumber);
-    void updateLocalParams(const std::string &SN, Camera *pCamera); // 更新本地对应SN相机参数
+    bool addCamera(const std::string &snNumber, BGAPI2::Device *dev);
 
   private:
     // 以下是sdk中的指针树结构，我尝试过很多次保存指针来管理对象的方式，均以异常结果。
     // 故只能沿用其sdk中对于对象指针的管理和索引方式。无法重构其数据结构。
-    std::shared_mutex mtxSN_;
-    std::list<std::string> lvSNNumber;
-    std::shared_mutex mtxCamera_;
-    std::unordered_map<std::string, Camera *> mapCameras_;
-    std::unordered_map<std::string, Json::Value> mapParams_;
-    std::atomic_bool managerRun_{true};
-    std::thread thSearchDev_;
-    std::unordered_map<std::string, CameraParams> mapStringToParams_;
-    std::unordered_map<CameraParams, std::string> mapParamsToString_;
+    std::mutex mtxCamera_;
+    std::vector<Json::Value> lvParams_;
+    std::vector<std::string> lvSNNumber_;
+    std::vector<Camera *> lvCameras_;
+    BGAPI2::System *pSystem_ = nullptr;
+    BGAPI2::Interface *pInterface_ = nullptr;
+    std::string MACAddress;
 };
