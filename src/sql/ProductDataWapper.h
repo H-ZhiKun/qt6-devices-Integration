59341a48a954b74078016a81e391ac15d732212b
502401467 <952007485@qq.com>
2023-09-22 18:17:15 +0800

    change productData insert

--- src/sql/ProductDataWapper.h
+++ src/sql/ProductDataWapper.h
@@ -16,23 +16,41 @@ class ProductDataWapper
     ~ProductDataWapper() = default;
     static bool insert(std::shared_ptr<ProductItem> ptr)
     {
-        Json::Value jsonValue;
-        TypeProduct type = ptr->typePd_;
-        jsonValue["typePd_"] = static_cast<uint8_t>(type);
-        jsonValue["bottle_num"] = ptr->bottleNum_;
-        jsonValue["batch_num"] = ptr->batchNum_;
-        jsonValue["formula_name"] = ptr->formulaName_;
-        jsonValue["is_complete"] = ptr->isComplete_;
-        jsonValue["qrcode_result"] = ptr->QRCode;
-        jsonValue["logistics_true_value_1"] = ptr->logistics1;
-        jsonValue["logistics_true_value_2"] = ptr->logistics2;
-        jsonValue["location_image_path"] = ptr->LocationPath;
-        jsonValue["check_image_path"] = ptr->CheckPath;
-        jsonValue["ocr_image_path"] = ptr->OCRPath;
-        jsonValue["location_result"] = ptr->LocationResult;
-        jsonValue["check_result"] = ptr->CheckResult;
-        jsonValue["ocr_result"] = ptr->OCRResult;
-        bool res = PgsqlHelper::getSqlHelper().insertData(TABLE_PRODUCT_DATA, std::move(jsonValue));
+
+        std::string sql =
+            fmt::format("INSERT INTO product_time (typePd_, bottle_num, batch_num, formula_name, is_complete,\
+        qrcode_result, logistics_true_value_1, \
+        logistics_true_value_2, location_image_path, \
+         check_image_path, ocr_image_path, \
+         location_result, check_result,\
+         ocr_result) VALUES (\
+           {}, '{}', '{}', '{}', {}, \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}';",
+                        static_cast<uint8_t>(ptr->typePd_), ptr->bottleNum_, ptr->batchNum_, ptr->formulaName_,
+                        ptr->isComplete_, ptr->QRCode, ptr->logistics1, ptr->logistics2, ptr->LocationPath,
+                        ptr->CheckPath, ptr->OCRPath, ptr->LocationResult, ptr->CheckResult, ptr->OCRResult);
+
+        // Json::Value jsonValue;
+        // TypeProduct type = ptr->typePd_;
+        // jsonValue["typePd_"] = static_cast<uint8_t>(type);
+        // jsonValue["bottle_num"] = ptr->bottleNum_;
+        // jsonValue["batch_num"] = ptr->batchNum_;
+        // jsonValue["formula_name"] = ptr->formulaName_;
+        // jsonValue["is_complete"] = ptr->isComplete_;
+        // jsonValue["qrcode_result"] = ptr->QRCode;
+        // jsonValue["logistics_true_value_1"] = ptr->logistics1;
+        // jsonValue["logistics_true_value_2"] = ptr->logistics2;
+        // jsonValue["location_image_path"] = ptr->LocationPath;
+        // jsonValue["check_image_path"] = ptr->CheckPath;
+        // jsonValue["ocr_image_path"] = ptr->OCRPath;
+        // jsonValue["location_result"] = ptr->LocationResult;
+        // jsonValue["check_result"] = ptr->CheckResult;
+        // jsonValue["ocr_result"] = ptr->OCRResult;
+        bool res = PgsqlHelper::getSqlHelper().insertData(TABLE_PRODUCT_DATA, std::move(sql));
         return res;
     }
 };
--- src/sql/ProductTimeWapper.h
+++ src/sql/ProductTimeWapper.h
@@ -16,29 +16,51 @@ class ProductTimeWapper
     ~ProductTimeWapper() = default;
     static bool insert(std::shared_ptr<ProductItem> ptr)
     {
-        Json::Value jsonValue;
-        TypeProduct type = ptr->typePd_;
-        jsonValue["typePd_"] = static_cast<uint8_t>(type);
-        jsonValue["bottle_num"] = ptr->bottleNum_;
-        jsonValue["batch_num"] = ptr->batchNum_;
-        jsonValue["formula_name"] = ptr->formulaName_;
-        jsonValue["is_complete"] = ptr->isComplete_;
-        jsonValue["qrcode_signal_time"] = ptr->QRSigTime;
-        jsonValue["qrcode_time"] = ptr->QRCodeTime;
-        jsonValue["logistics_ret_time"] = ptr->logisticsTime;
-        jsonValue["location_signal_time"] = ptr->LocationSigTime;
-        jsonValue["location_image_time"] = ptr->LocationImageTime;
-        jsonValue["location_result_time"] = ptr->LocationResultTime;
-        jsonValue["check_signal_time"] = ptr->CheckSigTime;
-        jsonValue["check_image_time"] = ptr->CheckImageTime;
-        jsonValue["check_result_time"] = ptr->CheckResultTime;
-        jsonValue["coding_signal_time"] = ptr->codingSigTime;
-        jsonValue["ocr_signal_time"] = ptr->OCRSigTime;
-        jsonValue["ocr_image_time"] = ptr->OCRImageTime;
-        jsonValue["ocr_result_time"] = ptr->OCRResultTime;
-        jsonValue["complete_signal_time"] = ptr->completeSigTime;
+        std::string sql = fmt::format(
+            "INSERT INTO product_time (typePd_, bottle_num, batch_num, formula_name, is_complete,\
+        qrcode_signal_time, qrcode_time, \
+        logistics_ret_time, location_signal_time, \
+        location_image_time, location_result_time,\
+         check_signal_time, check_image_time, \
+         check_result_time, coding_signal_time,\
+         ocr_signal_time, ocr_image_time, \
+         ocr_result_time, complete_signal_time) VALUES (\
+           {}, '{}', '{}', '{}', {}, \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}', \
+           '{}', '{}';",
+            static_cast<uint8_t>(ptr->typePd_), ptr->bottleNum_, ptr->batchNum_, ptr->formulaName_, ptr->isComplete_,
+            ptr->QRSigTime, ptr->QRCodeTime, ptr->logisticsTime, ptr->LocationSigTime, ptr->LocationImageTime,
+            ptr->LocationResultTime, ptr->CheckSigTime, ptr->CheckImageTime, ptr->CheckResultTime, ptr->codingSigTime,
+            ptr->OCRSigTime, ptr->OCRImageTime, ptr->OCRResultTime, ptr->completeSigTime);
+
+        // Json::Value jsonValue;
+        // TypeProduct type = ptr->typePd_;
+        // jsonValue["typePd_"] = static_cast<uint8_t>(type);
+        // jsonValue["bottle_num"] = ptr->bottleNum_;
+        // jsonValue["batch_num"] = ptr->batchNum_;
+        // jsonValue["formula_name"] = ptr->formulaName_;
+        // jsonValue["is_complete"] = ptr->isComplete_;
+        // jsonValue["qrcode_signal_time"] = ptr->QRSigTime;
+        // jsonValue["qrcode_time"] = ptr->QRCodeTime;
+        // jsonValue["logistics_ret_time"] = ptr->logisticsTime;
+        // jsonValue["location_signal_time"] = ptr->LocationSigTime;
+        // jsonValue["location_image_time"] = ptr->LocationImageTime;
+        // jsonValue["location_result_time"] = ptr->LocationResultTime;
+        // jsonValue["check_signal_time"] = ptr->CheckSigTime;
+        // jsonValue["check_image_time"] = ptr->CheckImageTime;
+        // jsonValue["check_result_time"] = ptr->CheckResultTime;
+        // jsonValue["coding_signal_time"] = ptr->codingSigTime;
+        // jsonValue["ocr_signal_time"] = ptr->OCRSigTime;
+        // jsonValue["ocr_image_time"] = ptr->OCRImageTime;
+        // jsonValue["ocr_result_time"] = ptr->OCRResultTime;
+        // jsonValue["complete_signal_time"] = ptr->completeSigTime;
         // Json::Value jsonValue = Utils::stringToJson(jsonString.toStdString());
-        bool res = PgsqlHelper::getSqlHelper().insertData(TABLE_PRODUCT_TIME, std::move(jsonValue));
+        bool res = PgsqlHelper::getSqlHelper().insertData(TABLE_PRODUCT_TIME, std::move(sql));
         return res;
     }
 };
